0251db2b0f78e2c55139043d63b158ab369e09c2
girumee <grmzegeye@gmail.com>
2023-03-16 14:41:54 +0300

    counter added in to result page on 16-03-2023

--- src/Components/Home/ResultsAndChoosUS/ChooseUs.jsx
+++ src/Components/Home/ResultsAndChoosUS/ChooseUs.jsx
@@ -1,16 +1,3 @@
-export default function ChooseUs (){
-    return <div className="w-full h-screen flex">
-        <div className='w-1/2 text-left pl-20 py-20'>
-            <p>Why choose us?</p>
-            <p className='pt-5 w-4/5 text-lg text-justify'>Lorem ipsum dolor sit amet consectetur adipiscing elit ultrices scelerisque mi sed interdum lacus tellus in mi orc, netus nisl laoreet phasellus. Pellentesque non nunc placerat mi quis vitae cursus ornare.</p>
-            <button className='rounded-full bg-[#962DFF] py-3 px-5 text-white text-lg mt-5'>Learn More</button>
-            <div className='w-5/6 h-1/2 mt-14'>
-            <iframe className="h-full w-full rounded-xl"
-                src={`https://www.youtube.com/embed/TMKJiVxrEqI`}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="Embedded youtube"></iframe>
-            </div>
 export default function ChooseUs() {
   return (
     <div className="w-full h-screen flex">
